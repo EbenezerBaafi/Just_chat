4da984753df45885318264c4a81d401270aada6b
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2026-04-07 17:13:32 +0000

    Add middleware (CORS)
    Update INSTALLED_APPS
    
    Configure DRF + JWT

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    bio = models.TextField(blank=True)
+
+    is_online = models.BooleanField(default=False)
+    last_seen = models.DateTimeField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
