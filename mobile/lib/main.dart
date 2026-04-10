046741aad54c1bea2aec2809002ce45272b3611f
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2026-04-10 15:12:21 +0000

    Added a chat screen which opens the dm of two users

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/auth_provider.dart';
 import 'screens/login_screen.dart';
-import 'screens/user_list_screen.dart';
+import 'screens/contact_screen.dart';
 
 void main() {
   runApp(
--- mobile/lib/services/api_service.dart
+++ mobile/lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 class ApiService {
   final Dio _dio = Dio(
     BaseOptions(
-      baseUrl: "http://10.194.233.60:8000/api/",
+      baseUrl: "http://10.212.40.60/api/",
       headers: {"Content-Type": "application/json"},
       connectTimeout: const Duration(seconds: 10),
       receiveTimeout: const Duration(seconds: 10),
@@ -29,6 +29,36 @@ class ApiService {
   Future<Response> getUsers(String token) async {
     return await _dio.get(
       "/users/all/",
+      options: Options(headers: {"Authorization": "Bearer $token"}),
+    );
+  }
+
+  // START OR GET CONVERSATION
+  Future<Response> startConversation(String token, int userId) async {
+    return await _dio.post(
+      "/chat/conversations/start/",
+      data: {"user_id": userId},
+      options: Options(headers: {"Authorization": "Bearer $token"}),
+    );
+  }
+
+  // GET MESSAGES
+  Future<Response> getMessages(String token, int conversationId) async {
+    return await _dio.get(
+      "/chat/conversations/$conversationId/messages/",
+      options: Options(headers: {"Authorization": "Bearer $token"}),
+    );
+  }
+
+  // SEND MESSAGE
+  Future<Response> sendMessage(
+    String token,
+    int conversationId,
+    String content,
+  ) async {
+    return await _dio.post(
+      "/chat/conversations/$conversationId/send/",
+      data: {"content": content},
       options: Options(headers: {"Authorization": "Bearer $token"}),
     );
   }
